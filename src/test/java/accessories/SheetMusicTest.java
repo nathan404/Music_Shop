74b8d33d0b90e23a2bed9104af6af771baad397b
Nathan <nathankirley@gmail.com>
2020-06-26 17:03:41 +0100

    tests written for sheet music constructors

--- src/test/java/accessories/SheetMusicTest.java
+++ src/test/java/accessories/SheetMusicTest.java
@@ -1,5 +1,8 @@
 package accessories;
 
+import enums.MaterialType;
+import enums.PianoType;
+import instruments.Piano;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,14 +10,37 @@ import static org.junit.Assert.assertEquals;
 
 public class SheetMusicTest {
 
-//    @Before
-//    public void before(){
-//
-//    }
-//
-//    @Test
-//    public void hasName(){
-//        assertEquals("", .getName());
-//    }
+    private Piano piano;
+    private SheetMusic sheetMusic;
 
+    @Before
+    public void before(){
+        piano = new Piano("Bechstein", MaterialType.PINE, 4000, 6000, PianoType.GRAND, 88);
+        sheetMusic = new SheetMusic("Romance Larghetto", 6, 10, "Chopin", 5, piano);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Romance Larghetto", sheetMusic.getName());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(6, sheetMusic.getBuyingPrice());
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(10, sheetMusic.getSellingPrice());
+    }
+
+    @Test
+    public void hasComposer(){
+        assertEquals("Chopin", sheetMusic.getComposer());
+    }
+
+    @Test
+    public void hasInstrument(){
+        assertEquals(piano, sheetMusic.getInstrument());
+    }
 }
