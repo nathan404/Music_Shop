ddfc47a02cdfb73f9282db5a27be0e66de296200
Nathan <nathankirley@gmail.com>
2020-06-26 16:56:41 +0100

    constructor tests written for bow class

--- src/test/java/accessories/BowTest.java
+++ src/test/java/accessories/BowTest.java
@@ -1,5 +1,9 @@
 package accessories;
 
+import enums.MaterialType;
+import enums.StringType;
+import enums.ViolinType;
+import instruments.Violin;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,13 +11,31 @@ import static org.junit.Assert.assertEquals;
 
 public class BowTest {
 
-//    @Before
-//    public void before(){
-//
-//    }
-//
-//    @Test
-//    public void hasName(){
-//        assertEquals("", .getName());
-//    }
+    private Bow bow;
+
+    @Before
+    public void before(){
+        Violin violin = new Violin("Stradivarius", MaterialType.SPRUCE, 1000000, 2000000, ViolinType.CLASSICAL, StringType.GUT);
+        bow = new Bow("Beginner's Bow", 300, 500, violin);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Beginner's Bow", bow.getName());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(300, bow.getBuyingPrice());
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(500, bow.getSellingPrice());
+    }
+
+    @Test
+    public void hasStringInstrument(){
+        assertEquals(violin, bow.getStringInstrument());
+    }
 }
