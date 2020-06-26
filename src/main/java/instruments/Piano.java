2f6ce1173d9bbbf98ab282af7ebd9b6e9892da40
Nathan <nathankirley@gmail.com>
2020-06-26 16:52:19 +0100

    piano constructor tests pass

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,24 @@
 package instruments;
 
+import enums.MaterialType;
+import enums.PianoType;
+
 public class Piano extends Instrument{
+
+    private PianoType pianoType;
+    private int numberOfKeys;
+
+    public Piano(String name, MaterialType materialType, int buyingPrice, int sellingPrice, PianoType pianoType, int numberOfKeys) {
+        super(name, materialType, buyingPrice, sellingPrice);
+        this.pianoType = pianoType;
+        this.numberOfKeys = numberOfKeys;
+    }
+
+    public PianoType getPianoType() {
+        return pianoType;
+    }
+
+    public int getNumberOfKeys() {
+        return numberOfKeys;
+    }
 }
