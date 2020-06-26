98183a468ff4c6021cbdd0d72566df55004653b8
Nathan <nathankirley@gmail.com>
2020-06-26 15:53:28 +0100

    guitarType enum written

--- src/main/java/enums/GuitarType.java
+++ src/main/java/enums/GuitarType.java
@@ -1,4 +1,18 @@
 package enums;
 
 public enum GuitarType {
+    ACOUSTIC(19),
+    CLASSICAL(19),
+    JAZZ(19),
+    ELECTRIC(24);
+
+    private final int fretNumber;
+
+    GuitarType(int fretNumber){
+        this.fretNumber = fretNumber;
+    }
+
+    public int getFretNumber(){
+        return fretNumber;
+    }
 }
