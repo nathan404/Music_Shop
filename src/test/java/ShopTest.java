44d5f9e48c57e52d65cf613fbefdfb4a677e9e93
Nathan <nathankirley@gmail.com>
2020-06-26 21:28:27 +0100

    canAddToTill test written

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -64,4 +64,10 @@ public class ShopTest {
         assertEquals(4, shop.getStockCount());
     }
 
+    @Test
+    public void canAddToTill(){
+        shop.addToTill(1000);
+        assertEquals(21000, shop.getTill());
+    }
+
 }
