b060980f9e95e2e5d3249d58086ccb908aefe79a
Nathan <nathankirley@gmail.com>
2020-06-26 17:04:46 +0100

    sheetmusic constructor tests pass

--- src/main/java/accessories/SheetMusic.java
+++ src/main/java/accessories/SheetMusic.java
@@ -1,4 +1,29 @@
 package accessories;
 
+import instruments.Instrument;
+
 public class SheetMusic extends Accessory {
+
+    private String composer;
+    private int numberOfPages;
+    private Instrument instrument;
+
+    public SheetMusic(String name, int buyingPrice, int sellingPrice, String composer, int numberOfPages, Instrument instrument) {
+        super(name, buyingPrice, sellingPrice);
+        this.composer = composer;
+        this.numberOfPages = numberOfPages;
+        this.instrument = instrument;
+    }
+
+    public String getComposer() {
+        return composer;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public Instrument getInstrument() {
+        return instrument;
+    }
 }
