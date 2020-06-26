dc514792797d4c18d5427986a4c77b5ec9ca6162
Nathan <nathankirley@gmail.com>
2020-06-26 16:03:35 +0100

    instrument abstract class initially set up

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -9,4 +9,26 @@ public abstract class Instrument {
     private int buyingPrice;
     private int sellingPrice;
 
+    public Instrument(String name, MaterialType materialType, int buyingPrice, int sellingPrice) {
+        this.name = name;
+        this.materialType = materialType;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MaterialType getMaterialType() {
+        return materialType;
+    }
+
+    public int getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public int getSellingPrice() {
+        return sellingPrice;
+    }
 }
