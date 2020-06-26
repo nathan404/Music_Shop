ad2bfdb7434d67308554af2ecb916152d9f12d87
Nathan <nathankirley@gmail.com>
2020-06-26 16:17:22 +0100

    violin class constructed

--- src/main/java/instruments/Violin.java
+++ src/main/java/instruments/Violin.java
@@ -1,4 +1,25 @@
 package instruments;
 
-//public class Violin extends StringInstrument{
-//}
+import enums.MaterialType;
+import enums.StringType;
+import enums.ViolinType;
+
+public class Violin extends StringInstrument{
+
+    private ViolinType violinType;
+    private StringType stringType;
+
+    public Violin(String name, MaterialType materialType, int buyingPrice, int sellingPrice, int numberOfStrings, boolean bow, ViolinType violinType, StringType stringType) {
+        super(name, materialType, buyingPrice, sellingPrice, numberOfStrings, bow);
+        this.violinType = violinType;
+        this.stringType = stringType;
+    }
+
+    public ViolinType getViolinType() {
+        return violinType;
+    }
+
+    public StringType getStringType() {
+        return stringType;
+    }
+}
--- src/test/java/instruments/ViolinTest.java
+++ src/test/java/instruments/ViolinTest.java
@@ -1,18 +1,59 @@
 package instruments;
 
+import enums.CelloType;
+import enums.MaterialType;
+import enums.StringType;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ViolinTest {
-//    @Before
-//    public void before(){
-//
-//    }
-//
-//    @Test
-//    public void hasName(){
-//        assertEquals("", .getName());
-//    }
+
+    private Violin violin;
+
+    @Before
+    public void before(){
+        cello = new Cello("Yamaha", MaterialType.WILLOW, 800, 1200, 4, true, CelloType.PERFORMER, StringType.GUT);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Yamaha", cello.getName());
+    }
+
+    @Test
+    public void hasMaterialType(){
+        assertEquals(MaterialType.WILLOW, cello.getMaterialType());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(800, cello.getBuyingPrice());
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(1200, cello.getSellingPrice());
+    }
+
+    @Test
+    public void hasNumberOfStrings(){
+        assertEquals(4, cello.getNumberOfStrings());
+    }
+
+    @Test
+    public void hasBow(){
+        assertEquals(true, cello.hasBow());
+    }
+
+    @Test
+    public void hasCelloType(){
+        assertEquals(CelloType.PERFORMER, cello.getCelloType());
+    }
+
+    @Test
+    public void hasStringType(){
+        assertEquals(StringType.GUT, cello.getStringType());
+    }
 }
