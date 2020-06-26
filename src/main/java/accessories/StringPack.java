e8eb551489d6df4de0e006df0a9f23b4ad0b4c9d
Nathan <nathankirley@gmail.com>
2020-06-26 17:08:53 +0100

    stringpack constructor tests pass

--- src/main/java/accessories/StringPack.java
+++ src/main/java/accessories/StringPack.java
@@ -1,4 +1,18 @@
 package accessories;
 
+import instruments.StringInstrument;
+
 public class StringPack extends Accessory {
+
+    private StringInstrument stringInstrument;
+
+
+    public StringPack(String name, int buyingPrice, int sellingPrice, StringInstrument stringInstrument) {
+        super(name, buyingPrice, sellingPrice);
+        this.stringInstrument = stringInstrument;
+    }
+
+    public StringInstrument getStringInstrument() {
+        return stringInstrument;
+    }
 }
