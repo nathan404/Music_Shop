9a622125a0a2f76a7baceb50dd015525c89c035f
Nathan <nathankirley@gmail.com>
2020-06-26 17:12:14 +0100

    ISell interface written

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -4,14 +4,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ShopTest {
+
+//    private Shop shop;
+//
 //    @Before
 //    public void before(){
-//
+//        shop = new Shop("Mikey's Music Shop", 20000, stock);
 //    }
 //
 //    @Test
 //    public void hasName(){
-//        assertEquals("", .getName());
+//        assertEquals("Mikey's Music Shop", shop.getName());
+//    }
+//
+//    @Test
+//    public void hasTill(){
+//        assertEquals(20000, shop.getTill());
 //    }
 
 }
