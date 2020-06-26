a13dc548c0090dcc30c76dc5158a3c3630880785
Nathan <nathankirley@gmail.com>
2020-06-26 15:46:46 +0100

    hasName test written for cello

--- src/test/java/instruments/CelloTest.java
+++ src/test/java/instruments/CelloTest.java
@@ -1,5 +1,7 @@
 package instruments;
 
+import enums.MaterialType;
+import enums.StringType;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,14 +9,16 @@ import static org.junit.Assert.assertEquals;
 
 public class CelloTest {
 
+    private Cello cello;
+
     @Before
     public void before(){
-
+        cello = new Cello("Yamaha", MaterialType.WOOD, 800, 1200, 4, true, StringType.GUT);
     }
 
     @Test
     public void hasName(){
-        assertEquals("", .getName());
+        assertEquals("Yamaha", cello.getName());
     }
 
 
