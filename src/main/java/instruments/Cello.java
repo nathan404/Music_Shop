4a550c000a51aed9d3f090cc836f58e96282bbe4
Nathan <nathankirley@gmail.com>
2020-06-26 16:09:00 +0100

    cello class initially set up

--- src/main/java/instruments/Cello.java
+++ src/main/java/instruments/Cello.java
@@ -1,4 +1,25 @@
 package instruments;
 
+import enums.CelloType;
+import enums.MaterialType;
+import enums.StringType;
+
 public class Cello extends StringInstrument {
+
+    private CelloType celloType;
+    private StringType stringType;
+
+    public Cello(String name, MaterialType materialType, int buyingPrice, int sellingPrice, int numberOfStrings, boolean bow, CelloType celloType, StringType stringType) {
+        super(name, materialType, buyingPrice, sellingPrice, numberOfStrings, bow);
+        this.celloType = celloType;
+        this.stringType = stringType;
+    }
+
+    public CelloType getCelloType() {
+        return celloType;
+    }
+
+    public StringType getStringType() {
+        return stringType;
+    }
 }
